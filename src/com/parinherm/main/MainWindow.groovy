a2cac395db749d798e76fda73b98930ee71be782
paul martin <mussym@live.com>
2019-10-28 10:58:41 -0500

    first commit

---
+++ src/com/parinherm/main/MainWindow.groovy
@@ -0,0 +1,15 @@
+package com.parinherm.main
+
+import org.eclipse.jface.window.ApplicationWindow
+
+class MainWindow extends ApplicationWindow {
+	
+	MainWindow() {
+		super(null)
+	}
+	
+	
+	static void main(String... args) {
+		println "Im here bitches"
+	}	
+}
