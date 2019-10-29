c4188c3d35cc5cc482e12d1e7247b58e1f7c9bdc
paul martin <mussym@live.com>
2019-10-28 23:23:02 -0500

    main menu will not show up, nor toolbar or status line

--- src/com/parinherm/main/MainWindow.groovy
+++ src/com/parinherm/main/MainWindow.groovy
@@ -2,6 +2,7 @@ package com.parinherm.main
 
 import org.eclipse.core.databinding.observable.Realm
 import org.eclipse.jface.action.MenuManager
+import org.eclipse.jface.action.Separator
 import org.eclipse.jface.action.StatusLineManager
 import org.eclipse.jface.action.ToolBarManager
 import org.eclipse.jface.databinding.swt.DisplayRealm
@@ -16,6 +17,21 @@ import org.eclipse.swt.widgets.Composite
 import org.eclipse.swt.widgets.Control
 import org.eclipse.swt.widgets.Display
 import org.eclipse.swt.widgets.Shell
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IAction;
+
+
+import org.eclipse.jface.layout.GridDataFactory
+import org.eclipse.swt.custom.CTabFolder
+import org.eclipse.swt.custom.CTabItem
+import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.swt.events.SelectionListener
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.layout.GridLayout
+import org.eclipse.swt.widgets.Event
+import org.eclipse.swt.widgets.Listener
+import org.eclipse.swt.widgets.ToolBar
 
 import com.parinherm.ui.view.DataBindingTest
 
@@ -26,12 +42,18 @@ class MainWindow extends ApplicationWindow {
 	private final static String IMAGE_ACTIVITY_LARGE = "activitylarge"
 	private final static String IMAGES_PATH = "/images/"
 	
+	private IAction exitAction = new Action("E&xit\tCtrl+X") {
+		void run() {
+			close()
+		}
+	}
+	
 	MainWindow() {
 		super(null)
-		setupImages()
-		addToolBar(SWT.FLAT | SWT.WRAP)
-		addMenuBar()
-		addStatusLine()
+		this.setupImages()
+		this.addMenuBar()
+		this.addToolBar(SWT.FLAT | SWT.WRAP)
+		this.addStatusLine()
 	}
 	
 	private def setupImages() {
@@ -44,29 +66,56 @@ class MainWindow extends ApplicationWindow {
 	
 	/* overrides */
 	
-	@Override
 	protected Control createContents(Composite parent) {
+		
+
+		
+		parent.setLayout(new FillLayout())
 		def container = new Composite(parent, SWT.NONE)
 		container.setLayout(new FillLayout())
 		def dbTest = new DataBindingTest(container)
-		return container
+		setStatus("what in the hell?")
+		container
 	}
 	
-	@Override
-	protected MenuManager createMenuManager() {
-		def mm = new MenuManager("menu")
-		return mm
+	MenuManager createMenuManager() {
+		
+		try {
+			println "create the goddamned"
+			MenuManager mm = new MenuManager("menu")
+
+			
+			MenuManager fileMenu = new MenuManager("&File")
+			fileMenu.add(new Separator())
+			fileMenu.add(exitAction)
+			mm.add(fileMenu)
+			
+			MenuManager helpMenu = new MenuManager("&Help")
+			//helpMenu.add(ApplicationData.instance().getAction(ApplicationData.ABOUT_ACTION_KEY));
+			mm.add(helpMenu);
+			
+			mm.update(true)
+			mm
+		}
+		catch (e) {
+			println e
+		}
 	}
 	
-	@Override
 	protected ToolBarManager createToolBarManager(int style) {
 		def tbm = new ToolBarManager(SWT.NONE)
-		return tbm
+		
+		ActionContributionItem item = new ActionContributionItem(exitAction)
+		item.setMode(ActionContributionItem.MODE_FORCE_TEXT)
+		tbm.add(item);
+		
+		tbm.update(true)
+		tbm
 	}
 	
-	@Override
 	protected StatusLineManager createStatusLineManager() {
-		return new StatusLineManager()
+		def sl = new StatusLineManager()
+		sl
 	}
 	
 	void configureShell(Shell newShell) {
@@ -82,6 +131,7 @@ class MainWindow extends ApplicationWindow {
 	}
 	
 	public boolean close() {
+		//close an resources here
 		return super.close()
 	}
 	
@@ -92,6 +142,8 @@ class MainWindow extends ApplicationWindow {
 			try {
 				def mainwin = new MainWindow()
 				mainwin.setBlockOnOpen(true)
+
+				
 				mainwin.open()
 				Display.getCurrent().dispose()
 			} catch (e) {
