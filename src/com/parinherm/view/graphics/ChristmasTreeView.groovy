7bdab5392b371c189c6ff57fba367740dc45969a
paul martin <mussym@live.com>
2019-12-06 10:52:43 -0600

    example from gef book

--- src/com/parinherm/view/graphics/ChristmasTreeView.groovy
+++ src/com/parinherm/view/graphics/ChristmasTreeView.groovy
@@ -1,12 +1,20 @@
 package com.parinherm.view.graphics
 
+
 import org.eclipse.draw2d.ColorConstants
 import org.eclipse.draw2d.Figure
 import org.eclipse.draw2d.IFigure
+import org.eclipse.draw2d.Label
 import org.eclipse.draw2d.LightweightSystem
+import org.eclipse.draw2d.PolygonShape
 import org.eclipse.draw2d.RectangleFigure
+import org.eclipse.draw2d.ToolbarLayout
 import org.eclipse.draw2d.XYLayout
+import org.eclipse.draw2d.geometry.PrecisionPoint
+import org.eclipse.draw2d.geometry.Rectangle
 import org.eclipse.swt.SWT
+import org.eclipse.swt.layout.GridData
+import org.eclipse.swt.layout.GridLayout
 import org.eclipse.swt.widgets.Canvas
 import org.eclipse.swt.widgets.Composite
 
@@ -26,27 +34,59 @@ class ChristmasTreeView {
 
 		//new Figure
 		//def canvas = new Canvas(parent, SWT.NONE)
+		parent.setLayout(new GridLayout())
 		Canvas canvas = createDiagram(parent)
-		
+		canvas.setLayoutData(new GridData(GridData.FILL_BOTH))
 
 	}
 	
 	private Canvas createDiagram(Composite parent) {
 		Figure root = new Figure()
 		root.setFont(parent.getFont())
-		XYLayout layout = new XYLayout()	
+		XYLayout layout = new XYLayout()
 		root.setLayoutManager(layout)
+		
+		def andy = createPersonFigure("Andy")
+		root.add(andy, new Rectangle(new PrecisionPoint(10.0d, 10.0d), andy.getPreferredSize()))
+		def betty = createPersonFigure("Betty")
+		root.add(betty, new Rectangle(new PrecisionPoint(230.0d, 10.0d), betty.getPreferredSize()))
+		def carl = createPersonFigure("Carl")
+		root.add(carl, new Rectangle(new PrecisionPoint(120.0d, 120.0d), betty.getPreferredSize()))
+		
+		def marriage = createMarriageFigure()
+		root.add(marriage, new Rectangle(new PrecisionPoint(145.9d, 35.0d), marriage.getPreferredSize()))
+		
 		Canvas canvas = new Canvas(parent, SWT.DOUBLE_BUFFERED)
 		canvas.setBackground(ColorConstants.white)
 		LightweightSystem lws = new LightweightSystem(canvas)
 		lws.setContents(root)
+		canvas
 	}
 	
 	private IFigure createPersonFigure(String name) {
 		def rectangleFigure = new RectangleFigure()
 		rectangleFigure.setBackgroundColor(ColorConstants.lightGray)
-		
+		rectangleFigure.setLayoutManager(new ToolbarLayout())
+		rectangleFigure.setPreferredSize(100, 100)
+		rectangleFigure.add(new Label(name))
+		rectangleFigure
 			
+	}
+	
+	private IFigure createMarriageFigure() {
+		Rectangle r = new Rectangle(0, 0, 50, 50)
+		PolygonShape ps = new PolygonShape()
+		ps.setStart(r.getTop())
+		ps.addPoint(r.getTop())
+		ps.addPoint(r.getLeft())
+		ps.addPoint(r.getBottom())
+		ps.addPoint(r.getRight())
+		ps.addPoint(r.getTop())
+		ps.setEnd(r.getTop())
+		ps.setFill(true)
+		ps.setBackgroundColor(ColorConstants.lightGray)
+		ps.setPreferredSize(r.getSize())
+		ps
 	}
 	
 	
