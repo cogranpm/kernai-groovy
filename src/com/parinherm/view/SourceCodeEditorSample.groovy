79a53ccb3c21c0321703bccb5d2dff73a98ab776
paul martin <mussym@live.com>
2019-12-22 15:18:12 -0600

    get rid of the text stuff, won't work in standalone mode

--- src/com/parinherm/view/SourceCodeEditorSample.groovy
+++ src/com/parinherm/view/SourceCodeEditorSample.groovy
@@ -2,7 +2,6 @@ package com.parinherm.view
 
 import org.eclipse.swt.widgets.Composite
 import org.eclipse.swt.SWT
-import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor
 
 class SourceCodeEditorSample extends Composite{
 	
